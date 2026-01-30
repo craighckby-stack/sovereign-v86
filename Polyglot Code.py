ac0fc815a03604fc46c246c2cb4010865fd7dbf9
CRAIG HUCKERBY <craighckby@gmail.com>
2026-01-30 20:46:34 +1000

    Update Polyglot Code.py

--- Polyglot Code.py
+++ Polyglot Code.py
@@ -1,2 +1,6 @@
 Write a file that prints "Hello" in Python, "World" in C, and "!" in Java—all using the same text.
 ​The Trick: It uses the different ways languages handle comments. For example, # is a comment in Python but a "Pre-processor" command in C.
+
+
+s = 's = %r\nprint(s %% s)'
+print(s % s)

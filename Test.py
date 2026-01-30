ae29de72128f951bc42adc6acaa81d9b348b62a3
CRAIG HUCKERBY <craighckby@gmail.com>
2026-01-30 20:30:26 +1000

    [Sovereign v86] Optimized Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,29 @@
 import sys
 
 def trace_killer(frame, event, arg):
-    # Detects if a debugger is watching
+    """
+    A tracing function to potentially interfere with debugging by deleting specific local variables.
+
+    This function is called by sys.settrace() for every event in a traced thread.
+    If a tracer is set (sys.gettrace() is not None), this function will attempt to
+    delete 'my_secret_variable' from the local variables of the current frame.
+    """
+    # It's slightly more robust to check if tracing is active, although the caller
+    # (the Python interpreter during tracing) implies it is.
     if sys.gettrace() is not None:
-        # Delete the variable you are trying to inspect
-        if 'my_secret_variable' in frame.f_locals:
-            del frame.f_locals['my_secret_variable']
+        variable_to_hide = 'my_secret_variable'
+        if variable_to_hide in frame.f_locals:
+            try:
+                # Attempt to delete the variable from the frame's locals dictionary
+                del frame.f_locals[variable_to_hide]
+            except Exception:
+                # In rare cases (like built-in types or specific frame configurations),
+                # deletion might fail. We suppress errors here as the goal is to obscure.
+                pass
+
+    # A tracing function must always return itself to continue tracing.
     return trace_killer
 
+# Set the system tracer function
+# Note: Setting a trace function globally affects all threads unless used with threading.settrace.
 sys.settrace(trace_killer)

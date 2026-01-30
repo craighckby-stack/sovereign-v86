28ec715bbc02c3267651f8b1836375ccc966855e
CRAIG HUCKERBY <craighckby@gmail.com>
2026-01-30 20:44:43 +1000

    Create Ouroboros challenge.py

---
+++ Ouroboros challenge.py
@@ -0,0 +1,4 @@
+​You run a Ruby program.
+​The output is a Python program.
+​The Python program's output is a Java program.
+​...and after 100 different languages, the final output is the original Ruby code.

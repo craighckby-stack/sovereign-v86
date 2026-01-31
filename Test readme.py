a11a8c4473780180aa5a94745f003ec516ca78b9
CRAIG HUCKERBY <craighckby@gmail.com>
2026-01-31 12:48:33 +1000

    [Sovereign] Sync: Test readme.py

--- Test readme.py
+++ Test readme.py
@@ -1,8 +1,4 @@
-Correction: It must be import sys (lowercase i).
-
-Import sys  # <--- Python is case-sensitive. This will fail.
-
-
-# The Counter-Measure
 import sys
-sys.settrace(None)  # Detaches the hostile trace_killer
+
+# Counter-measure: Detach any active tracing function (e.g., debuggers or profilers)
+sys.settrace(None)
